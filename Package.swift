69388267e49f65c2715f0e163f2d2074503fdcdb
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:45:52 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -14,12 +14,12 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-entitlement",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .macCatalyst(.v26),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .macCatalyst(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
